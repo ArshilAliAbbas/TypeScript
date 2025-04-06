63e199abb7b2caa513849829adfecaf14f29e1a9
ArshilAliAbbas <arshil1227@gmail.com>
2025-04-06 00:35:14 -0400

    tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,52 +2,46 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[10px_1fr_1000px] justify-items-center min-h-screen p-8 pb-20 gap-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[40px] row-start-1 items-center justify-items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          className="mx-auto"
+          src="/rmgName.png"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={500}
+          height={500}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
+        <ol className=" flex items-center justify-center justify-items-center list-inside list-decimal text-sm/15 text-center sm:text-left font-[family-name:var(--font-geist-mono)] bg-name gap-1">
+          <ul className="mb-10 tracking-[.8em] text-center">
+            Student | Entrepreneur | Developer
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold "></code>
+          </ul>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="mx-auto flex gap-4 items-center justify-items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-1 hover:bg-[#E4EFE7] dark:hover:bg-[#E4EFE7] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto "
+            href="https://www.linkedin.com/in/arshilaliabbas/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className=""
+              src="/wr.png"
+              alt="linkdlnlogo"
+              width={30}
+              height={30}
             />
-            Deploy now
+            LinkedIn
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#E83F25] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-10 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://linktr.ee/Arshil.io"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener noreferrer" //page security.
           >
-            Read our docs
+            About Me.
           </a>
         </div>
       </main>
@@ -65,7 +59,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Instagram
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -80,7 +74,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          G i t h u b
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -95,7 +89,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          M o n e y
         </a>
       </footer>
     </div>
